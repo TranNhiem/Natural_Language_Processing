af932335edc707d41cad6d2aa898b77a074483d9
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-10-21 12:26:34 +0800

    commit

---
+++ NER/Processing_data/data_utils.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import tensorflow as tf 
+
+## The goal of this function Convert text data into CSV similar format--> 
+# Better for --> Implement tf.data.Dataset 
+def convert_text_csv_format(export): 
+    
