42e5968e644f4c8d6f994a350a8c063d5f303cf7
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-21 14:56:15 +0800

    commit

---
+++ Image_retrival_using_text_query/config/config.py
@@ -0,0 +1,80 @@
+from config.absl_mock import Mock_Flag
+
+
+def read_cfg(mod="non_contrastive"):
+    flags = Mock_Flag()
+    base_cfg()
+    wandb_set()
+
+
+def base_cfg():
+    flags = Mock_Flag()
+
+    flags.DEFINE_integer(
+        'IMG_HEIGHT', 224,
+        'image height.')
+
+    flags.DEFINE_integer(
+        'IMG_WIDTH', 224,
+        'image width.')
+
+    flags.DEFINE_integer(
+        'SEED', 26,  # 40, 26
+        'random seed use for shuffle data Generate two same image ds_one & ds_two')
+
+    flags.DEFINE_integer(
+        'training_samples', 40.000,
+        'size of training dataset MS-COCO 80K')
+
+    flags.DEFINE_integer(
+        'val_samples', 5000,
+        'size of validation dataset MS-COCO val or sperate from Train data.')
+
+    flags.DEFINE_integer(
+        'training_samples', 40.000,
+        'size of training dataset MS-COCO 80K')
+
+    flags.DEFINE_integer(
+        'images_per_file', 2000,
+        'Splitting train folder each subset train folders.')
+
+    flags.DEFINE_float(
+        'num_captions', 2,
+        'Number of captions will make training sample increasing X times')
+
+    flags.DEFINE_integer(
+        'train_batch_size', 200,
+        'Train batch_size .')
+
+    flags.DEFINE_integer(
+        'val_batch_size', 200,
+        'Validaion_Batch_size.')
+
+    flags.DEFINE_integer(
+        'train_epochs', 100,
+        'Number of epochs to train for.')
+
+    flags.DEFINE_string(
+        
+        'train_path', '/data1/share/1K_New/train/',
+        'Train dataset path.')
+
+    flags.DEFINE_string(
+        # 'val_path',"/mnt/sharefolder/Datasets/SSL_dataset/ImageNet/1K_New/val",
+        'val_path', "/data1/share/1K_New/val/",
+        'Validaion dataset path.')
+
+
+def wandb_set():
+    flags = Mock_Flag()
+    flags.DEFINE_string(
+        "wandb_project_name", "NLP_finetune_BERT_Architecture",
+        "set the project name for wandb."
+    )
+    flags.DEFINE_string(
+        "wandb_run_name", "Image_retrival_from_Text_Query",
+        "set the run name for wandb."
+    )
+    flags.DEFINE_enum(
+        'wandb_mod', 'run', ['run', 'dryrun'],
+        'update the to the wandb server or not')
--- Image_retrival_using_text_query/run_model.py
+++ Image_retrival_using_text_query/run_model.py
@@ -1,32 +1,31 @@
 import os
-import collections
 import json
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-from absl import flags
-from data_loader import write_data
-from absl import logging
+import collections
 from absl import app
+from absl import flags
+import tensorflow as tf
+from absl import logging
+from data_loader import write_data
+from tensorflow.keras import layers
 from model_architecture import project_embeddings, create_vision_encoder, create_text_encoder
 
+# ************************************************
+# Devices Configures
+# ************************************************
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        tf.config.experimental.set_visible_devices(gpus[0:8], 'GPU')
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        print(e)
+
 FLAGS = flags.FLASG
-
-flags.DEFINE_integer(
-    'training_samples', 40.000,
-    'size of training dataset MS-COCO 80K')
-
-flags.DEFINE_integer(
-    'val_samples', 5000,
-    'size of validation dataset MS-COCO val or sperate from Train data.')
-
-flags.DEFINE_integer(
-    'images_per_file', 2000,
-    'Splitting train folder each subset train folders.')
-
-flags.DEFINE_float(
-    'num_captions', 2,
-    'Number of captions will make training sample increasing X times')
 
 
 def main(argv):
