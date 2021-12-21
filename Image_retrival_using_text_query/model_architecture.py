c3fdfb02f0dc93ca2e04d1153f692c7ad7d3c1bb
TranNhiem <86524375+TranNhiem@users.noreply.github.com>
2021-12-21 11:35:31 +0800

    commit

--- Image_retrival_using_text_query/model_architecture.py
+++ Image_retrival_using_text_query/model_architecture.py
@@ -4,6 +4,18 @@ import tensorflow_text as text
 import tensorflow_addons as tfa
 from tqdm import tqdm
 
+'''
+Description: Implementation of a dual encoder model for retrieving images that match natural language queries.
+
+ View in Colab • GitHub source
+
+Introduction
+The example demonstrates how to build a dual encoder (also known as two-tower) neural network model to search for images using natural language. The model is inspired by the CLIP approach, introduced by Alec Radford et al. The idea is to train a vision encoder and a text encoder jointly to project the representation of images and their captions into the same embedding space, such that the caption embeddings are located near the embeddings of the images they describe.
+
+This example requires TensorFlow 2.4 or higher. In addition, TensorFlow Hub and TensorFlow Text are required for the BERT model, and TensorFlow Addons is required for the AdamW optimizer. These libraries can be installed using the following command:
+
+'''
+
 def project_embeddings(
     embeddings, num_projection_layers, projection_dims, dropout_rate
 ):
@@ -18,42 +30,9 @@ def project_embeddings(
 
 
  
-About Keras
-Getting started
-Developer guides
-Keras API reference
-Code examples
-Computer Vision
-Natural Language Processing
-Structured Data
-Timeseries
-Audio Data
-Generative Deep Learning
-Reinforcement Learning
-Graph Data
-Quick Keras Recipes
-Why choose Keras?
-Community & governance
-Contributing to Keras
-KerasTuner
-Search Keras documentation...
 
-» Code examples / Natural Language Processing / Natural language image search with a Dual Encoder
-Natural language image search with a Dual Encoder
-Author: Khalid Salama
-Date created: 2021/01/30
-Last modified: 2021/01/30
-Description: Implementation of a dual encoder model for retrieving images that match natural language queries.
 
- View in Colab • GitHub source
 
-Introduction
-The example demonstrates how to build a dual encoder (also known as two-tower) neural network model to search for images using natural language. The model is inspired by the CLIP approach, introduced by Alec Radford et al. The idea is to train a vision encoder and a text encoder jointly to project the representation of images and their captions into the same embedding space, such that the caption embeddings are located near the embeddings of the images they describe.
-
-This example requires TensorFlow 2.4 or higher. In addition, TensorFlow Hub and TensorFlow Text are required for the BERT model, and TensorFlow Addons is required for the AdamW optimizer. These libraries can be installed using the following command:
-
-pip install -q -U tensorflow-hub tensorflow-text tensorflow-addons
-Setup
 import os
 import collections
 import json
@@ -68,162 +47,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from tqdm import tqdm
 
-# Suppressing tf.hub warnings
-tf.get_logger().setLevel("ERROR")
-Prepare the data
-We will use the MS-COCO dataset to train our dual encoder model. MS-COCO contains over 82,000 images, each of which has at least 5 different caption annotations. The dataset is usually used for image captioning tasks, but we can repurpose the image-caption pairs to train our dual encoder model for image search.
-
-Download and extract the data
-
-First, let's download the dataset, which consists of two compressed folders: one with images, and the other—with associated image captions. Note that the compressed images folder is 13GB in size.
-
-root_dir = "datasets"
-annotations_dir = os.path.join(root_dir, "annotations")
-images_dir = os.path.join(root_dir, "train2014")
-tfrecords_dir = os.path.join(root_dir, "tfrecords")
-annotation_file = os.path.join(annotations_dir, "captions_train2014.json")
-
-# Download caption annotation files
-if not os.path.exists(annotations_dir):
-    annotation_zip = tf.keras.utils.get_file(
-        "captions.zip",
-        cache_dir=os.path.abspath("."),
-        origin="http://images.cocodataset.org/annotations/annotations_trainval2014.zip",
-        extract=True,
-    )
-    os.remove(annotation_zip)
-
-# Download image files
-if not os.path.exists(images_dir):
-    image_zip = tf.keras.utils.get_file(
-        "train2014.zip",
-        cache_dir=os.path.abspath("."),
-        origin="http://images.cocodataset.org/zips/train2014.zip",
-        extract=True,
-    )
-    os.remove(image_zip)
-
-print("Dataset is downloaded and extracted successfully.")
-
-with open(annotation_file, "r") as f:
-    annotations = json.load(f)["annotations"]
-
-image_path_to_caption = collections.defaultdict(list)
-for element in annotations:
-    caption = f"{element['caption'].lower().rstrip('.')}"
-    image_path = images_dir + "/COCO_train2014_" + "%012d.jpg" % (element["image_id"])
-    image_path_to_caption[image_path].append(caption)
-
-image_paths = list(image_path_to_caption.keys())
-print(f"Number of images: {len(image_paths)}")
-Downloading data from http://images.cocodataset.org/annotations/annotations_trainval2014.zip
-252878848/252872794 [==============================] - 5s 0us/step
-Downloading data from http://images.cocodataset.org/zips/train2014.zip
-13510574080/13510573713 [==============================] - 394s 0us/step
-Dataset is downloaded and extracted successfully.
-Number of images: 82783
-Process and save the data to TFRecord files
-You can change the sample_size parameter to control many image-caption pairs will be used for training the dual encoder model. In this example we set train_size to 30,000 images, which is about 35% of the dataset. We use 2 captions for each image, thus producing 60,000 image-caption pairs. The size of the training set affects the quality of the produced encoders, but more examples would lead to longer training time.
-
-train_size = 30000
-valid_size = 5000
-captions_per_image = 2
-images_per_file = 2000
-
-train_image_paths = image_paths[:train_size]
-num_train_files = int(np.ceil(train_size / images_per_file))
-train_files_prefix = os.path.join(tfrecords_dir, "train")
-
-valid_image_paths = image_paths[-valid_size:]
-num_valid_files = int(np.ceil(valid_size / images_per_file))
-valid_files_prefix = os.path.join(tfrecords_dir, "valid")
-
-tf.io.gfile.makedirs(tfrecords_dir)
-
-
-def bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-def create_example(image_path, caption):
-    feature = {
-        "caption": bytes_feature(caption.encode()),
-        "raw_image": bytes_feature(tf.io.read_file(image_path).numpy()),
-    }
-    return tf.train.Example(features=tf.train.Features(feature=feature))
-
-
-def write_tfrecords(file_name, image_paths):
-    caption_list = []
-    image_path_list = []
-    for image_path in image_paths:
-        captions = image_path_to_caption[image_path][:captions_per_image]
-        caption_list.extend(captions)
-        image_path_list.extend([image_path] * len(captions))
-
-    with tf.io.TFRecordWriter(file_name) as writer:
-        for example_idx in range(len(image_path_list)):
-            example = create_example(
-                image_path_list[example_idx], caption_list[example_idx]
-            )
-            writer.write(example.SerializeToString())
-    return example_idx + 1
-
-
-def write_data(image_paths, num_files, files_prefix):
-    example_counter = 0
-    for file_idx in tqdm(range(num_files)):
-        file_name = files_prefix + "-%02d.tfrecord" % (file_idx)
-        start_idx = images_per_file * file_idx
-        end_idx = start_idx + images_per_file
-        example_counter += write_tfrecords(file_name, image_paths[start_idx:end_idx])
-    return example_counter
-
-
-train_example_count = write_data(train_image_paths, num_train_files, train_files_prefix)
-print(f"{train_example_count} training examples were written to tfrecord files.")
-
-valid_example_count = write_data(valid_image_paths, num_valid_files, valid_files_prefix)
-print(f"{valid_example_count} evaluation examples were written to tfrecord files.")
-100%|███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 15/15 [03:19<00:00, 13.27s/it]
-  0%|                                                                                                                                     | 0/3 [00:00<?, ?it/s]
-
-60000 training examples were written to tfrecord files.
-
-100%|█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 3/3 [00:33<00:00, 11.07s/it]
-
-10000 evaluation examples were written to tfrecord files.
-Create tf.data.Dataset for training and evaluation
-feature_description = {
-    "caption": tf.io.FixedLenFeature([], tf.string),
-    "raw_image": tf.io.FixedLenFeature([], tf.string),
-}
-
-
-def read_example(example):
-    features = tf.io.parse_single_example(example, feature_description)
-    raw_image = features.pop("raw_image")
-    features["image"] = tf.image.resize(
-        tf.image.decode_jpeg(raw_image, channels=3), size=(299, 299)
-    )
-    return features
-
-
-def get_dataset(file_pattern, batch_size):
-
-    return (
-        tf.data.TFRecordDataset(tf.data.Dataset.list_files(file_pattern))
-        .map(
-            read_example,
-            num_parallel_calls=tf.data.AUTOTUNE,
-            deterministic=False,
-        )
-        .shuffle(batch_size * 10)
-        .prefetch(buffer_size=tf.data.AUTOTUNE)
-        .batch(batch_size)
-    )
-Implement the projection head
-The projection head is used to transform the image and the text embeddings to the same embedding space with the same dimensionality.
 
 def project_embeddings(
     embeddings, num_projection_layers, projection_dims, dropout_rate
@@ -239,8 +62,8 @@ def project_embeddings(
 '''
 Implement the vision encoder
 In this example, we use Xception from Keras Applications as the base for the vision encoder.
-
 '''
+
 def create_vision_encoder(
     num_projection_layers, projection_dims, dropout_rate, trainable=False
 ):
